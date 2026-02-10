9be724faa5248a767163db3abaa7f495d35afcc9
JosCapdev <josecapdevila512@gmail.com>
2026-02-10 20:37:09 -0300

    fix: ajustes en AuthenticationController
    - Se modificó la lógica del controlador de autenticación
    - Actualización de métodos/endpoints para mejorar el flujo de login

--- src/main/java/ForoHub/api/controller/AuthenticationController.java
+++ src/main/java/ForoHub/api/controller/AuthenticationController.java
@@ -1,8 +1,16 @@
 package ForoHub.api.controller;
 
+import ForoHub.api.auth.dto.DatosAuthentication;
+import ForoHub.api.domain.usuario.model.Usuario;
+import ForoHub.api.infra.security.DatosTokenJWT;
 import ForoHub.api.infra.security.TokenService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +20,14 @@ public class AuthenticationController {
     @Autowired
     private TokenService tokenService;
     @Autowired
-    private AuthenticationManager authenticationManager;
+    private AuthenticationManager manager;
+
+    @PostMapping
+    public ResponseEntity iniciarSesion(@RequestBody @Valid DatosAuthentication datos){
+        var authenticationToken = new UsernamePasswordAuthenticationToken(datos.email(),datos.pass());
+        var autenticacion = manager.authenticate(authenticationToken);
+        var tokenJWT = tokenService.generarToken((Usuario) autenticacion.getPrincipal());
+
+        return ResponseEntity.ok(new DatosTokenJWT(tokenJWT));
+    }
 }
