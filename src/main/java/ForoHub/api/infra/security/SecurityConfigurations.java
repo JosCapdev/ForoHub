5e4689c82612ca189e9d86046cc82507744ff345
JosCapdev <josecapdevila512@gmail.com>
2026-02-18 14:04:57 -0300

    feat: configurar documentación con springdoc y actualizar dependencias

--- src/main/java/ForoHub/api/infra/security/SecurityConfigurations.java
+++ src/main/java/ForoHub/api/infra/security/SecurityConfigurations.java
@@ -28,6 +28,7 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers(HttpMethod.POST,"/login").permitAll()
                             .requestMatchers(HttpMethod.POST,"/usuario").permitAll()
+                            .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
                             .anyRequest().authenticated();
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
